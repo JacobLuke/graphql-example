a69239c5622d1e522ba7180ddb6cbeeb6b33ddad
JacobLuke <jacob.luke.abrahams@gmail.com>
2020-01-28 11:40:22 -0500

    Resolvers for computed fields

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const { Units, WorkOrders, Listings } = require('./data');
 
 const typeDefs = parse(readFileSync('./schema.graphql', 'UTF-8'));
 
-// TODO
 const resolvers = {
     Query: {
         workOrder: (_, args) => findById(WorkOrders, args.id),
         listing: (_, args) => findById(Listings, args.id),
+    },
+    WorkOrder: {
+        unit: (parent) => findById(Units, parent.unit),
+        activeListing: (parent) => Listings.find(l =>
+            l.workOrder === parent.id &&
+            l.status !== "Removed" &&
+            l.status !== "Cancelled" &&
+            l.status !== "NotSold"
+        ),
+    },
+    Listing: {
+        workOrder: (parent) => findById(WorkOrders, parent.workOrder),
+    },
+    Unit: {
+        activeWorkOrder: (parent) => WorkOrders.find(w =>
+            w.unit === parent.id && w.status !== "Inactive",
+        ),
     }
 };
 
